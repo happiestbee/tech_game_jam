15993dadc8e28e8b43c8c2c11cdb1acbc92dcded
happiestbee <87628038+happiestbee@users.noreply.github.com>
2021-07-18 23:27:16 -0400

    Fixed Pycharm

--- main.py
+++ main.py
@@ -2,5 +2,19 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((770,510))
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('A bit Racey')
+clock = pygame.time.Clock()
 
+crashed = False
+
+while not crashed:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
