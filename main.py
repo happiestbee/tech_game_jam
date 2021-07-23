ec7f4236116c1a3c58461f383d95f39cfc4b4552
happiestbee <87628038+happiestbee@users.noreply.github.com>
2021-07-23 10:21:02 -0400

    Added Graphics

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ from sys import exit
 import math
 from random import randint
 
+
 class Health(pygame.sprite.Sprite):
     def __init__(self,x,y,immortal):
         super().__init__()
-        self.image = pygame.Surface((30,30)).convert_alpha()
         if immortal == 0:
-            self.image.fill((0,255,0))
+            self.image = pygame.image.load('Assets/Graphics/heart.png').convert_alpha()
         else:
-            self.image.fill((0,255,255))
+            self.image = pygame.image.load('Assets/Graphics/absorption.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (30,30))
         self.rect = self.image.get_rect(topleft=(x,y))
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, wall_list, bullet_list, enemy_list, x, y):
         super().__init__()
         self.original_image = pygame.Surface((40,40), pygame.SRCALPHA)
         self.original_image.fill((0,0,0))
-        self.image = self.original_image
+        self.image = pygame.image.load('Assets/Graphics/player_sprite.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image,(40,40))
         self.rect = self.image.get_rect(topleft = (x,y))
         self.angle = -90
         self.speed = 3
@@ -29,7 +32,7 @@ class Player(pygame.sprite.Sprite):
 
         self.bullet_list = bullet_list
         self.bullet_speed = 5
-        self.bullet_size = 5
+        self.bullet_size = 10
         self.reload_speed = 300
         self.can_fire = True
         self.fire_time = 0
@@ -126,22 +129,34 @@ class Player(pygame.sprite.Sprite):
         self.enemy_collision()
         self.display_health()
 
+
 class Wall(pygame.sprite.Sprite):
     def __init__(self,x,y):
         super().__init__()
-        self.image = pygame.Surface((40,40)).convert_alpha()
-        self.image.fill((150,150,150))
+        stone_bricks = pygame.image.load('Assets/Graphics/polished_blackstone_bricks.png')
+        stone_bricks = pygame.transform.scale(stone_bricks,(40,40))
+        self.image = stone_bricks.convert_alpha()
         self.rect = self.image.get_rect(topleft=(x,y))
         self.x = x
         self.y = y
+
+
+class Floor(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        stone = pygame.image.load('Assets/Graphics/stone.png')
+        stone = pygame.transform.scale(stone, (40, 40))
+        self.image = stone.convert_alpha()
+        self.rect = self.image.get_rect(topleft=(x,y))
+
 
 class Player_Bullet(pygame.sprite.Sprite):
     def __init__(self,size,speed,player_rect):
         super().__init__()
         self.size = size
         self.speed = speed
-        self.image = pygame.Surface((self.size,self.size))
-        self.image.fill((0,255,0))
+        self.image = pygame.image.load('Assets/Graphics/player_bullet.png')
+        self.image = pygame.transform.scale(self.image,(self.size,self.size))
         self.rect = self.image.get_rect(center=(player_rect.center))
         x, y = pygame.mouse.get_pos()
         x_1, y_1 = self.rect.center
@@ -168,11 +183,12 @@ class Player_Bullet(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+
 class Test_Enemy(pygame.sprite.Sprite):
     def __init__(self,x,y,wall_list,player_bullets):
         super().__init__()
-        self.image = pygame.Surface((40,40)).convert_alpha()
-        self.image.fill((255,0,0))
+        self.image = pygame.image.load('Assets/Graphics/enemy1.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image,(40,40))
         self.rect = self.image.get_rect(topleft=(x,y))
 
         self.move_x = 0
@@ -233,11 +249,11 @@ class Test_Enemy(pygame.sprite.Sprite):
         self.player_bullet_collision()
 
 
-
 def load_level(level_array,player_group,wall_group,enemy_group,player_bullets):
     global enemy_count
     for i in range(20):
         for j in range(15):
+            floor.add(Floor(i * 40, j * 40))
             if level_array[j][i] == 'W':
                 # wall = pygame.Surface((40,40))
                 # wall.fill((255,0,0))
@@ -255,17 +271,17 @@ pygame.font.init()
 test_level = ["WWWWWWWWWWWWWWWWWWWW",
               'WXXXXXXXXXXXXXXXXXXW',
               'WXXXXXXXXXXXXXXXXXXW',
-              'WXXXXXXXX1XXXXXXXXXW',
-              'WXXX1XXXXXXXXXXXXXXW',
-              'WXXXXXXXXXXXXXX1XXXW',
+              'WXXXXXX1XXXXXXXXXXXW',
               'WXXXXXXXXXXXXXXXXXXW',
               'WXXXXXXXXXXXXXXXXXXW',
+              'WXX1XXXXXXXXXXXXXXXW',
+              'WXXXXXXXXXXXXXXXXPXW',
+              'WXXXXXXXXXXXXXXXXXXW',
+              'WXX1XXXXXXXXXXXXXXXW',
               'WXXXXXXXXXXXXXXXXXXW',
               'WXXXXXXXXXXXXXXXXXXW',
+              'WXXXXX1XXXXXXXXXXXXW',
               'WXXXXXXXXXXXXXXXXXXW',
-              'WXXXXXXXXXXXXXXXXXXW',
-              'WXXXXXXXXXXXXXXXXXXW',
-              'WPXXXXXXXXXXXXXXXXXW',
               'WWWWWWWWWWWWWWWWWWWW']
 
 walls = pygame.sprite.Group()
@@ -274,6 +290,8 @@ player_bullets = pygame.sprite.Group()
 player = pygame.sprite.GroupSingle()
 
 enemies = pygame.sprite.Group()
+
+floor = pygame.sprite.Group()
 
 health = pygame.sprite.Group()
 game_active = True
@@ -306,7 +324,7 @@ while True:
             exit()
 
     if game_active and enemy_count != 0:
-        screen.fill((100, 100, 100))
+        floor.draw(screen)
         walls.draw(screen)
         player.update()
         player.draw(screen)
